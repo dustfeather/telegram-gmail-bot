9cd6afcbcb6a948eedde32f6a07e2d9578a9bfd3
Catalin Teodorescu <dustfeather@gmail.com>
2024-05-27 08:06:09 +0300

    debugging packages on github

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="telegram-gmail-bot",
-    version="0.1.7",
+    version="0.1.8",
     author="Catalin Teodorescu",
     author_email="dustfeather@gmail.com",
     description="A bot that connects Telegram with Gmail",
